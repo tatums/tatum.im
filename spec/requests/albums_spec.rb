13d14cbc1b8fed009b223923b7b296933fb55de5
Tatum Szymczak <tatum@Tatum-Szymczaks-MacBook.local>
2012-09-17 08:57:58 -0500

    Adding album specs

--- spec/requests/albums_spec.rb
+++ spec/requests/albums_spec.rb
@@ -2,10 +2,25 @@ require 'spec_helper'
 
 describe "Albums" do
   describe "GET /albums" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    it "should show index page" do
       get albums_path
       response.status.should be(200)
+
     end
+
+    it "should display new album page" do
+      get new_album_path
+      response.should render_template(:new)
+    end
+
+    it "should create an Album" do
+      post "/albums", :album => {:name => "My Widget"}
+      response.should redirect_to(assigns(:album))
+      follow_redirect!
+
+      response.should render_template(:show)
+      response.body.should include("Album was successfully created.")
+    end
+
   end
 end
