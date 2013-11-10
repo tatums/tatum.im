63c46cc7c2928e575668ce757e13478d4de5f071
tatums <tatum@ashlandstudios.com>
2013-11-10 09:28:20 -0600

    add bourbon and change colors scheme

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :album_id, :image, :user_id, :title
+  attr_accessible :album_id, :image, :user_id, :title, :featured
   mount_uploader :image, ImageUploader
 
   belongs_to :album
