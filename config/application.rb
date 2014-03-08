738a7904ee41bf198b53319e841272b77a4b486c
tatums <tatum@ashlandstudios.com>
2014-03-08 16:45:02 -0600

    upgrade to rails 3.2.17 & add application setting for heroku

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,10 @@ module AshlandstudiosCom
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+
+    ## For Heroku
+    config.assets.initialize_on_precompile = false
+
   end
 end
