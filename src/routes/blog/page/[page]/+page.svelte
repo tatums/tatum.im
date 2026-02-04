b4b3f4358d77609f1ce31e9fd4371ba516f894a0
Tatum <tatum@ashlandstudios.com>
2026-02-04 08:20:40 -0500

    Fixes

--- src/routes/blog/page/[page]/+page.svelte
+++ src/routes/blog/page/[page]/+page.svelte
@@ -10,30 +10,32 @@
   <title>tatum.im</title>
 </svelte:head>
 
-{#each posts as post, index}
-  <article class="blog-post">
-    <header>
-      <h2>
-        <a href={post.postPath}>{post.title}</a>
-      </h2>
-      <span class="post-date">
-        <Fa icon={faCalendarAlt} />
-        { post.formattedDate }
-      </span>
-    </header>
-    <div> {@html post.html} </div>
-  </article>
+<main class="post">
+  {#each posts as post, index}
+    <article class="blog-post">
+      <header>
+        <h2>
+          <a href={post.postPath}>{post.title}</a>
+        </h2>
+        <span class="post-date">
+          <Fa icon={faCalendarAlt} />
+          { post.formattedDate }
+        </span>
+      </header>
+      <div> {@html post.html} </div>
+    </article>
 
-  {#if (index + 1) != posts.length}
-    <hr class="article" />
-  {/if}
-{/each}
+    {#if (index + 1) != posts.length}
+      <hr class="article" />
+    {/if}
+  {/each}
 
-<section class="pagination-controls">
-  <a href="/blog/page/{currentPage == 1 ? currentPage : currentPage - 1}" class="button prev-btn" disabled={currentPage == 1}>
-    <span>Previous</span>
-  </a>
-  <a href="/blog/page/{pagesCount == currentPage ? currentPage : currentPage + 1}" class="button next-btn" disabled={pagesCount == currentPage}>
-    <span>Next</span>
-  </a>
-</section>
+  <section class="pagination-controls">
+    <a href="/blog/page/{currentPage == 1 ? currentPage : currentPage - 1}" class="button prev-btn" disabled={currentPage == 1}>
+      <span>Previous</span>
+    </a>
+    <a href="/blog/page/{pagesCount == currentPage ? currentPage : currentPage + 1}" class="button next-btn" disabled={pagesCount == currentPage}>
+      <span>Next</span>
+    </a>
+  </section>
+</main>
--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -10,24 +10,26 @@
  <title>Posts</title>
 </svelte:head>
 
-<h2>Posts...</h2>
+<main class="post">
+  <h2>Posts...</h2>
 
-<section class="blog-posts">
-  <h2>Latest posts</h2>
+  <section class="blog-posts">
+    <h2>Latest posts</h2>
 
-    {#each posts as post, index}
-      <article class="blog-post">
-        <span class="post-date">{ post.formattedDate }</span>
-        <a href={post.postPath}>{post.title}</a>
-      </article>
-    {/each}
-</section>
+      {#each posts as post, index}
+        <article class="blog-post">
+          <span class="post-date">{ post.formattedDate }</span>
+          <a href={post.postPath}>{post.title}</a>
+        </article>
+      {/each}
+  </section>
 
-<section class="pagination-controls">
-  <a href="/blog/page/{currentPage == 1 ? currentPage : currentPage - 1}" class="button prev-btn" disabled={currentPage == 1}>
-    <span>Previous</span>
-  </a>
-  <a href="/blog/page/{pagesCount == currentPage ? currentPage : currentPage + 1}" class="button next-btn" disabled={pagesCount == currentPage}>
-    <span>Next</span>
-  </a>
-</section>
+  <section class="pagination-controls">
+    <a href="/blog/page/{currentPage == 1 ? currentPage : currentPage - 1}" class="button prev-btn" disabled={currentPage == 1}>
+      <span>Previous</span>
+    </a>
+    <a href="/blog/page/{pagesCount == currentPage ? currentPage : currentPage + 1}" class="button next-btn" disabled={pagesCount == currentPage}>
+      <span>Next</span>
+    </a>
+  </section>
+</main>
